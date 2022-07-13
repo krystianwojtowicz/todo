a0f0ea52297c2ca4e2583c682edf647a9b12ae7e
Krystian Wójtowicz <60481782+krystianwojtowicz@users.noreply.github.com>
2022-07-13 22:09:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const form = document.querySelector("form");
 const addTaskBtn = document.querySelector(".add-task");
 const close = document.querySelector(".close");
 const submit = document.querySelector(".submit");
-// const defaultProjects = document.querySelector(".defaultProject .wrapper-grid");
 const remove = document.querySelectorAll(".remove");
 const select = document.getElementById("projects");
 const projects = document.querySelector(".projects");
@@ -16,7 +15,6 @@ class Task {
     this.dueDate = dueDate;
     this.priority = priority;
     this.complete = false;
-    // this.id = new Date().valueOf();
   }
 }
 
@@ -27,201 +25,98 @@ class Project {
   }
 }
 const arrayOfProjects = JSON.parse(localStorage.getItem("array")) || [];
+let index = JSON.parse(localStorage.getItem("index")) || 0;
 
-// let task;
-// let newArr = [];
-// let project = [];
-// let divWrapper;
+(function () {
+  let project = [];
+  if (arrayOfProjects.length == 0) {
+    project = new Project("defaultProject");
+    arrayOfProjects.push(project);
+    console.log(arrayOfProjects);
+    console.log(JSON.parse(localStorage.getItem("arrayOfProjects")));
+    addOption(project);
+    console.log(arrayOfProjects);
+  } else {
+    arrayOfProjects.forEach((project) => {
+      addOption(project);
+      project.tasks.forEach(task => {
+        addTaskToDOM(task, project);
+      });
+    });
+  }
+})();
 
-if (arrayOfProjects.length == 0) {
-  let projectDefault = new Project("defaultProject");
-  arrayOfProjects.push(projectDefault);
-  console.log(arrayOfProjects);
-  console.log(JSON.parse(localStorage.getItem("arrayOfProjects")))
+function addOption(project) {
   let option = document.createElement("option");
-  option.value = arrayOfProjects[0].name;
-  option.textContent = arrayOfProjects[0].name;
-  select.options.add(
-    new Option(`${arrayOfProjects[0].name}`, `${arrayOfProjects[0].name}`)
-  );
-} else {
-  // display(addProjectToProjects, addTaskToProjects,);
+  option.value = project.name;
+  option.textContent = project.name;
+  select.options.add(new Option(`${project.name}`, `${project.name}`));
+  addProjectToDOM(project);
+  console.log(arrayOfProjects);
 }
-console.log(arrayOfProjects);
-
-// const arrayOfProjects = [];
-
-// let project = new Project('defaultProject');
-
-close.addEventListener("click", () => {
-  form.style.display = "none";
-});
-
-addTaskBtn.addEventListener("click", () => {
-  form.style.display = "block";
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  submit.addEventListener("click", addProjectToProjects);
-});
-
-// // bad
-// function foo() {
-//   // ...
-// }
-
-// // bad
-// const foo = function () {
-//   // ...
-// };
-
-// // good
-// // lexical name distinguished from the variable-referenced invocation(s)
-// const short = function longUniqueMoreDescriptiveLexicalFoo() {
-//   // ...
-// };
 
 function addProjectToProjects(e) {
-  let project = [];
-  let divWrapper;
   e.preventDefault();
-  let newArr = arrayOfProjects.map(
-    (project) => project[Object.keys(project)[0]]
-  );
+  let project = [];
   if (document.getElementById("project").value) {
     project = {
       name: document.getElementById("project").value,
       tasks: [],
     };
-  } else {
-    project = {
-      name: select.value,
-      tasks: [],
-    };
-  }
-// tu trzeba zrobić nową funkcję do wyświetlania projektu z localstorage i nie display tylko najpierw ta funkcja i tak samo z taskiem i powinno być ok
-  if (newArr.indexOf(project.name) > -1) {
-    // arrayOfProjects[newArr.indexOf(project.name)].tasks.push(task);
-    divWrapper = document.querySelector(
-      `.${arrayOfProjects[newArr.indexOf(project.name)].name} .wrapper-grid`
-    );
-  } else {
     arrayOfProjects.push(project);
-    // arrayOfProjects[arrayOfProjects.length - 1].tasks.push(task);
-    // console.log(arrayOfProjects);
-
-    const div = document.createElement("div");
-    div.classList.add(project.name);
-    projects.appendChild(div);
-
-    const h1 = document.createElement("h1");
-    h1.textContent = `Current Tasks in ${project.name}`;
-    div.appendChild(h1);
-
-    divWrapper = document.createElement("div");
-    divWrapper.classList.add("wrapper-grid");
-    div.appendChild(divWrapper);
-
-    option.value = arrayOfProjects[arrayOfProjects.length - 1].name;
-    option.textContent = arrayOfProjects[arrayOfProjects.length - 1].name;
-    select.options.add(
-      new Option(
-        `${arrayOfProjects[arrayOfProjects.length - 1].name}`,
-        `${arrayOfProjects[arrayOfProjects.length - 1].name}`
-      )
-    );
+    console.log(arrayOfProjects);
+    let myObj = JSON.stringify(arrayOfProjects);
+    localStorage.setItem("array", myObj);
+    addOption(project);
+    addTaskToProjects(project);
+  } else {
+    project.name = select.value;
+    console.log(project);
+    addTaskToProjects(project);
   }
-  newArr = arrayOfProjects.map((project) => project[Object.keys(project)[0]]);
-  // console.log(typeof project.name);
-  addTaskToProjects(newArr, divWrapper, project,);
 }
 
-let index = 0;
+function addProjectToDOM(project) {
+  const div = document.createElement("div");
+  div.classList.add(project.name);
+  projects.appendChild(div);
 
-function addTaskToProjects(newArr, divWrapper, project,) {
+  const h1 = document.createElement("h1");
+  h1.textContent = `Current Tasks in ${project.name}`;
+  div.appendChild(h1);
+
+  const divWrapper = document.createElement("div");
+  divWrapper.classList.add("wrapper-grid");
+  div.appendChild(divWrapper);
+}
+
+function addTaskToProjects(project) {
+  console.log(project.name)
   let task = {
+    id: index,
     title: document.getElementById("title").value,
     description: document.getElementById("description").value,
     dueDate: document.getElementById("dueDate").value,
     priority: document.getElementById("priority").value,
     complete: false,
-    // id: new Date().valueOf(),
-    id: index,
   };
-  index++;
+  index += 1;
+  let myIndex = JSON.stringify(index);
+    localStorage.setItem("index", myIndex);
+  arrayOfProjects[arrayOfProjects.findIndex(x => x.name === project.name)].tasks.push(task);
   console.log(arrayOfProjects);
-  console.log(arrayOfProjects[0].tasks);
-  arrayOfProjects[newArr.indexOf(project.name)].tasks.push(task);
   form.style.display = "none";
   form.reset();
-  const myObj = JSON.stringify(arrayOfProjects);
+  let myObj = JSON.stringify(arrayOfProjects);
   localStorage.setItem('array', myObj);
-  // const myObjd = JSON.parse(localStorage.getItem("arrayOfProjects"))
-  // console.log(myObjd);
-  display(newArr, divWrapper, project);
+  addTaskToDOM(task, project);
 }
-
-function display(newArr, divWrapper, project) {
-  document.querySelector(
-    `.${arrayOfProjects[newArr.indexOf(project.name)].name} .wrapper-grid`
-  ).textContent = "";
-  // let index = 0;
-  // console.log(newArr);
-  // console.log(arrayOfProjects[newArr.indexOf(project.name)]);
-  arrayOfProjects[newArr.indexOf(project.name)].tasks.forEach((task) => {
-
-    const div = document.createElement("div");
-    div.classList.add("container");
-    divWrapper.appendChild(div);
-    div.setAttribute("id", `${task.id}`);
-    div.appendChild(createTaskElement("h4", `title: ${task.title}`));
-    div.appendChild(createTaskElement("h4", `description: ${task.description}`));
-    div.appendChild(createTaskElement("h4", `dueDate: ${task.dueDate}`));
-
-    createPriorityElement(task, div);
-    createCheckingElement(task, div);
-
-    // create remove task btn and add class attribute for each array card
-    const removeTaskBtn = document.createElement("button");
-    removeTaskBtn.classList.add("remove-task-btn");
-    removeTaskBtn.textContent = "remove";
-
-    // link the data attribute of the remove button to the array and card
-    removeTaskBtn.dataset.linkedArray = task.id;
-    const link = removeTaskBtn.dataset.linkedArray
-    div.appendChild(removeTaskBtn);
-
-    // start event listener/remove array item from array and card from parent div via data link
-    removeTaskBtn.addEventListener(
-      "click",
-      removeTaskFromProjects.bind(null, newArr, divWrapper, project, task, link, div,)
-    );
-  });
-}
-
 
 function createTaskElement(el, content) {
   const elem = document.createElement(el);
   elem.textContent = content;
   return elem;
 }
-
-
-function removeTaskFromProjects(newArr, divWrapper, project, task, link, div,) {
-  let getTaskToRemove = link;
-
-  function find(task) {
-    return task.id == getTaskToRemove;
-  };
-  const taskId = (arrayOfProjects[newArr.indexOf(project.name)].tasks.findIndex(find));
-  arrayOfProjects[newArr.indexOf(project.name)].tasks.splice(
-    parseInt(taskId),
-    1
-  );
-  div.remove();
-  display(newArr, divWrapper, project,);
-}
-
 
 function createPriorityElement(task, div,) {
   const showPriority = document.createElement("h4");
@@ -248,7 +143,6 @@ function createPriorityElement(task, div,) {
   });
 }
 
-
 function createCheckingElement(task, div,) {
   const checkbox = document.createElement("button");
   if (task.complete) {
@@ -270,6 +164,65 @@ function createCheckingElement(task, div,) {
   // checkbox.textContent = book.read ? 'read' : 'not read';
 }
 
-clear.addEventListener('click', function () {
+function addTaskToDOM(task, project) {
+  console.log(project.name);
+  const divWrapper = document.querySelector(`.${project.name} .wrapper-grid`);
+  const div = document.createElement("div");
+    div.classList.add("container");
+    divWrapper.appendChild(div);
+    div.setAttribute("id", `${task.id}`);
+    div.appendChild(createTaskElement("h4", `title: ${task.title}`));
+    div.appendChild(createTaskElement("h4", `description: ${task.description}`));
+    div.appendChild(createTaskElement("h4", `dueDate: ${task.dueDate}`));
+    createPriorityElement(task, div);
+    createCheckingElement(task, div);
+        // create remove task btn and add class attribute for each array card
+        const removeTaskBtn = document.createElement("button");
+        removeTaskBtn.classList.add("remove-task-btn");
+        removeTaskBtn.textContent = "remove";
+    
+        // link the data attribute of the remove button to the array and card
+        removeTaskBtn.dataset.linkedArray = task.id;
+        const link = removeTaskBtn.dataset.linkedArray
+        div.appendChild(removeTaskBtn);
+    
+        // start event listener/remove array item from array and card from parent div via data link
+        removeTaskBtn.addEventListener(
+          "click",
+          removeTaskFromProjects.bind(null, divWrapper, project, task, link, div,)
+        );
+}
+
+function removeTaskFromProjects(divWrapper, project, task, link, div,) {
+  let getTaskToRemove = link;
+
+  function find(task) {
+    return task.id == getTaskToRemove;
+  };
+  const taskId = (arrayOfProjects[arrayOfProjects.findIndex(x => x.name === project.name)].tasks.findIndex(find));
+  console.log(taskId);
+  arrayOfProjects[arrayOfProjects.findIndex(x => x.name === project.name)].tasks.splice(
+    parseInt(taskId),
+    1
+  );
+  let myObj = JSON.stringify(arrayOfProjects);
+    localStorage.setItem("array", myObj);
+  console.log(arrayOfProjects)
+  div.remove();
+}
+
+clear.addEventListener("click", function () {
   localStorage.clear();
-})
+});
+
+close.addEventListener("click", () => {
+  form.style.display = "none";
+});
+
+addTaskBtn.addEventListener("click", () => {
+  form.style.display = "block";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  submit.addEventListener("click", addProjectToProjects);
+});
